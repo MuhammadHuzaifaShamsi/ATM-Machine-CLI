575f04fb1bf9382d6e8b897020b07c8ee3aebaf8
Muhammad Huzaifa <109722221+MuhammadHuzaifaShamsi@users.noreply.github.com>
2024-03-23 16:52:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,84 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-let atm = await inquirer.prompt([
-    {
-        name: "id",
-        type: "input",
-        message: "Enter Your ID:"
-    },
-    {
-        name: "pin",
-        type: "input",
-        message: "Enter Your PIN:"
-    },
-    {
-        name: "action",
-        type: "list",
-        choices: ["Deposit", "Withdraw", "Fast Cash"],
-        message: "Which action do you want to perform?"
-    },
-    {
-        name: "fastAmount",
-        type: "list",
-        choices: [1000, 5000, 10000, 20000],
-        message: "Select Your Amount",
-        when(atm) {
-            return atm.action == "Fast Cash";
+while (true) {
+    let atm_registry = await inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "Create An ID:"
+        },
+        {
+            name: "pin",
+            type: "input",
+            message: "Create A PIN:"
         }
-    },
-    {
-        name: "amount",
-        type: "number",
-        message: "Enter Your Amount:",
-        when(atm) {
-            return atm.action != "Fast Cash";
+    ]);
+    let c_id = atm_registry.id;
+    let c_pin = atm_registry.pin;
+    if (c_id && c_pin) {
+        console.log(chalk.green("Your account was successfully made! Please Proceed to Login:"));
+        while (true) {
+            let atm_ver = await inquirer.prompt([
+                {
+                    name: "id_ver",
+                    type: "input",
+                    message: "Enter Your ID:"
+                },
+                {
+                    name: "pin_ver",
+                    type: "input",
+                    message: "Enter Your PIN:"
+                }
+            ]);
+            let v_id = atm_ver.id_ver;
+            let v_pin = atm_ver.pin_ver;
+            if (c_id == v_id && c_pin == v_pin) {
+                console.log(chalk.magentaBright("Welcome to your account"));
+                while (true) {
+                    let atm = await inquirer.prompt([
+                        {
+                            name: "action",
+                            type: "list",
+                            choices: ["Deposit", "Withdraw"],
+                            message: "Which action do you want to perform?"
+                        },
+                        {
+                            name: "amount",
+                            type: "number",
+                            message: "Enter Your Amount:"
+                        }
+                    ]);
+                    if (atm && atm.amount) {
+                        const balance = Math.floor(Math.random() * 100000);
+                        console.log(chalk.blue(`\nYour Bank Balance: ${balance}`));
+                        let action = atm.action.toLowerCase();
+                        if (action == "deposit") {
+                            let result = balance + atm.amount;
+                            console.log(chalk.green(`Dear User, your amount have been deposited! \nRemaining bank balance: ${result}`));
+                        }
+                        else if (balance < atm.amount) {
+                            console.log(chalk.redBright("Insuffiecient Bank Balance!"));
+                        }
+                        else {
+                            let result = balance - atm.amount;
+                            console.log(chalk.green(`Dear User, the withdrawal was a success! \nRemaining bank balance: ${result}`));
+                        }
+                        break;
+                    }
+                    else {
+                        console.log(chalk.redBright("Please Enter A Correct Amount!"));
+                    }
+                }
+                break;
+            }
+            else {
+                console.log(chalk.redBright("Invalid PIN or ID! Please Try Again:"));
+            }
         }
-    }
-]);
-if (atm.id && atm.pin) {
-    const balance = Math.floor(Math.random() * 100000);
-    console.log(chalk.blue(`\nYour Bank Balance: ${balance}`));
-    let action = atm.action.toLowerCase();
-    if (action == "deposit") {
-        let result = balance + atm.amount;
-        console.log(chalk.green(`Dear User, your amount have been deposited! \nRemaining bank balance: ${result}`));
-    }
-    else if (action == "fast cash") {
-        let result = balance - atm.fastAmount;
-        console.log(chalk.green(`Dear User, your selected amount have been withdrawn! \nRemaining bank balance: ${result}`));
-    }
-    else if (balance < atm.amount) {
-        console.log(chalk.red("Insuffiecient Bank Balance!"));
+        break;
     }
     else {
-        let result = balance - atm.amount;
-        console.log(chalk.green(`Dear User, the withdrawal was a success! \nRemaining bank balance: ${result}`));
+        console.log(chalk.redBright("Kindly fill all fields !!"));
     }
-}
-else {
-    console.log(chalk.red("Invalid ID or PIN"));
 }
